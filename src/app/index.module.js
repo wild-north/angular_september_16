e04bc3946e52ab637fb2bcdce209f33aa88ec750
Sergey Strizhak <strizhak.sergey@gmail.com>
2016-10-22 15:35:59 +0300

    lesson13-02

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -6,7 +6,8 @@
     	'ui.router',
     	'ui.bootstrap',
     	'toastr',
-    	'ngStorage'
+    	'ngStorage',
+    	'ngResource'
     	]);
 
 })();
--- src/app/products/product-api.service.js
+++ src/app/products/product-api.service.js
@@ -6,23 +6,53 @@
     .service('ProductsAPI', Service);
 
   /** @ngInject */
-  function Service($http, $q) {
+  function Service($http, $q, $resource) {
 
     var products = [];
 
+
+
   	this.getAll = function() {
+      var url = '/app/json/:fileName';
+      // music_store.json
       var d = $q.defer();
       if (products.length) {
         d.resolve(products);
       } else {
-        $http({
-          method: 'GET',
-          url: '/app/json/music_store.json'
-        })
-        .then(function(response) {
-          products = response.data;
-          d.resolve(products);
+        var res = $resource(url, {}, {
+          getProducts: {
+            method: 'GET',
+            isArray: true
+          },
+          addProduct: {
+            method: 'POST',
+            isArray: true,
+            params: {
+              fileName: '@file'
+            }
+          },
         });
+
+
+        res.addProduct({ }, {id: 9999, text: 'lorem', file: 'music_store.json'})
+
+        res.getProducts({
+          fileName: 'music_store.json'
+        })
+        .$promise
+        .then(function(data) {
+          console.log(data);
+          d.resolve(data);
+        });
+
+        // $http({
+        //   method: 'GET',
+        //   url: url
+        // })
+        // .then(function(response) {
+        //   products = response.data;
+        //   d.resolve(products);
+        // });
       }
       return d.promise;
   	};
